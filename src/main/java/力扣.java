3ebebbbfb67eebb5482423746980547961f67c71
Shensiqi <2385727542@qq.com>
2021-12-05 15:37:46 +0800

    hot-fix commit

--- src/main/java/力扣.java
+++ src/main/java/力扣.java
@@ -6,6 +6,7 @@ public class СІПл {
     public static void main(String[] args) {
         Solution solution=new Solution();
         Solution solution2=new Solution();
+        System.out.println("Hello,hot-fix");
         solution.myPow(2,7);
     }
 }
