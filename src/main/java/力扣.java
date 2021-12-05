11556feffbf5f9aec3b824880e81d880f5912a38
Shensiqi <2385727542@qq.com>
2021-12-05 15:29:46 +0800

    first commit

--- src/main/java/力扣.java
+++ src/main/java/力扣.java
@@ -5,6 +5,7 @@
 public class СІПл {
     public static void main(String[] args) {
         Solution solution=new Solution();
+        Solution solution2=new Solution();
         solution.myPow(2,7);
     }
 }
